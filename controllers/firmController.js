1eab174a1d61bf24a6e63e95878c9fc6114a66ef
Harsha-Devoleper <harshavardhan9321@gmail.com>
2025-11-05 16:32:19 +0530

    added path module

--- controllers/firmController.js
+++ controllers/firmController.js
@@ -1,6 +1,7 @@
 const Firm = require('../models/Firm');
 const Vendor = require('../models/Vendor');
 const multer = require('multer');
+const path = require('path');
 
 
 const storage = multer.diskStorage({
@@ -20,15 +21,11 @@ const addFirm = async (req, res) => {
         const { firmName, area, category, region, offer } = req.body;
         console.log(firmName, area, category, region, offer);
         const image = req.file ? req.file.path : null;
-        console.log("a");
-
 
         const vendor = await Vendor.findById(req.vendorId);
-        console.log("b");
         if (!vendor) {
             return res.status(404).json({ error: "Vendor not found" });
         }
-        console.log("c");
         const firm = new Firm({
             firmName,
             area,
@@ -38,14 +35,10 @@ const addFirm = async (req, res) => {
             image,
             vendor: vendor._id
         });
-        console.log("Before saving:", firm);
-        console.log("1");
+        
         const savedFirm = await firm.save();
-        console.log("2");
         vendor.firm.push(savedFirm);
-        console.log("3");
         await vendor.save();
-        console.log("4");
 
         res.status(201).json({ message: 'Firm added successfully' });
     } catch (error) {
